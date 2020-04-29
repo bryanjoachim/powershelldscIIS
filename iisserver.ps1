2ec41601daa3556667b7f33b2eeefcbd20b801d5
bryanjoachim <33373881+bryanjoachim@users.noreply.github.com>
2020-04-29 14:18:47 -0400

    Add files via upload

---
+++ iisserver.ps1
@@ -0,0 +1,23 @@
+﻿Configuration MyWebServerCfg {
+ 
+    Import-DscResource -ModuleName PsDesiredStateConfiguration
+ 
+    Node 'localhost' {
+ 
+      
+        WindowsFeature IIS {
+            Ensure = "Present"
+            Name   = "Web-Server"
+        }
+ 
+        
+        WindowsFeature IIS-Tools {
+            Ensure    = "Present"
+            Name      = "Web-Mgmt-Tools"
+            DependsOn = "[WindowsFeature]IIS"
+        }
+ 
+    }
+}
+MyWebServerCfg -OutputPath "C:\Dsc"
+Start-DscConfiguration -Path "C:\DSC"
